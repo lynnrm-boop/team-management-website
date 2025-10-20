52b85bdf9747f1d229d266fdb43e607c2a8f5f3c
Lynn Mortensen <aidenlm9120@gmai.com>
2025-10-19 23:57:42 -0400

    edit app.js add adding task

--- public/app.js
+++ public/app.js
@@ -26,8 +26,18 @@ async function LoginPage(){ // start of function
 async function displayTask(taskData){ //start of function
     let task = document.createElement("div");
 
+    let taskName = document.createElement("p");
+    let taskGroup = document.createElement("p");
+    let taskDueDate = document.createElement("p");
+    let taskDiscription = document.createElement("p");
+
+    task.appendChild(taskName);
+    task.appendChild(taskGroup);
+    task.appendChild(taskDueDate);
+    task.appendChild(taskDiscription);
     
-    return(task);
+    document.getElementById("taskDisplay").appendChild(task);
+
 } //end of function
 
 async function makeTask(){ //start of function
@@ -39,12 +49,17 @@ async function makeTask(){ //start of function
     let taskDiscriptionInput = document.createElement("input");
     let submitInput = document.createElement("button");
 
+    form.style.position = "absolute"
     form.style.display = "grid";
     form.style.width = "50%";
     form.style.height = "75%";
     form.style.gridTemplateRows = "20% 20% 20% 20% 20%";
     form.style.backgroundColor = "black";
+    form.style.right = "25%";
+    form.style.top = "12.5%"
 
+
+    taskNameInput.setAttribute("type","text");
 
     form.appendChild(taskNameInput);
     form.appendChild(taskGroupInput);
@@ -52,10 +67,26 @@ async function makeTask(){ //start of function
     form.appendChild(taskDiscriptionInput);
     form.appendChild(submitInput);
 
-    submitInput.addEventListener("click",() => { // function start
-        this.parentNode.appendChild(displayTask())
+
+    submitInput.addEventListener("click",(e) => { // function start
+        e.preventDefault();
+
+        let input = {
+            Name:taskNameInput.value,
+            Group:taskGroupInput.value,
+            DueDate:taskDueDateInput.value,
+            Discription:taskDiscriptionInput.value   
+        };
+
+        displayTask(input);
+
+       
+        
+
     } //function end
 )
+
+    document.getElementById("root").appendChild(form);
     //todo make this data send to backend
 
 } // end of function
@@ -79,15 +110,32 @@ async function GetData(){ //start of functon
 async function HomePage(Logdata){
     document.getElementById("root").innerHTML = "";
 
-    let TaskDisplay = document.createElement("div");
+    let HomeRoot = document.createElement("div");
+    HomeRoot.style.width = "100vw";
+    HomeRoot.style.height = "100vh";
+    HomeRoot.setAttribute("id","HomeRoot");
 
-    for(let i = 0; i < taskList.length; i++){
+    let TaskDisplay = document.createElement("div");
+    TaskDisplay.setAttribute("id","taskDisplay")
+
+    // to do connect to backend
+   /* for(let i = 0; i < taskList.length; i++){
         TaskDisplay.appendChild(displayTask());
-    }
+    } */
 
     let MakeTaskButton = document.createElement("button"); // child of TaskDisplay not entire page
+    MakeTaskButton.innerText = "+";
+    MakeTaskButton.style.width = "25%";
+    MakeTaskButton.style.height = "25%";
+    MakeTaskButton.addEventListener("click", makeTask)
+
     let TrackTime = document.createElement("div");
     
+    TaskDisplay.appendChild(MakeTaskButton);
+    HomeRoot.appendChild(TaskDisplay);
+    HomeRoot.appendChild(TrackTime);
+
+    document.getElementById("root").appendChild(HomeRoot);
 
 }
 
